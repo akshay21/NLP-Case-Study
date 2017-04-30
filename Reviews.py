ef9249ce15bd4225989b3a25d9db034ebca10c0f
Akshay Gawali <akshay.jgd21@gmail.com>
2017-04-30 01:31:08 -0500

    Positive words and itensifiers.

--- Reviews.py
+++ Reviews.py
@@ -1,34 +1,36 @@
 import json
-import nltk
-from nltk.tokenize import wordpunct_tokenize, word_tokenize, sent_tokenize
+from nltk.tokenize import sent_tokenize
 from google.cloud import language
 
 
 #Creating semantics
 with open('semantics.json') as semantics_file:
-    semantics=json.load(semantics_file)
+    semantics = json.load(semantics_file)
 
-posWords= [semantics['positive'][i]['phrase'] for i in range(len(semantics['positive']))]
-posVals=[semantics['positive'][i]['value'] for i in range(len(semantics['positive']))]
+posWords = [semantics['positive'][i]['phrase'] for i in range(len(semantics['positive']))]
+posVals = [semantics['positive'][i]['value'] for i in range(len(semantics['positive']))]
 
-intesifiers=[semantics['intensifier'][i]['phrase'] for i in range(len(semantics['intensifier']))]
-intesifierVals=[semantics['intensifier'][i]['multiplier'] for i in range(len(semantics['intensifier']))]
+intensifiers = [semantics['intensifier'][i]['phrase'] for i in range(len(semantics['intensifier']))]
+intensifierVals = [semantics['intensifier'][i]['multiplier'] for i in range(len(semantics['intensifier']))]
 
 lang_client = language.Client()
 
 with open('reviews1.json') as data_file:
-    data =json.load(data_file)
-info=data['HotelInfo']
-hotel_name=info['Name']
-print  "Hotel Name: ",hotel_name
-reviews=data['Reviews']
+    data = json.load(data_file)
+info = data['HotelInfo']
+hotel_name = info['Name']
+print "Hotel Name: ",hotel_name
+reviews = data['Reviews']
 
-topic='breakfast'
-pos_count=0
-neg_count=0
-total_count=0
+topic = 'breakfast'
+pos_count = 0
+pos_val = 0.0
+neg_count = 0
+neg_val = 0.0
+total_count = 0
+
 def topic_finder():
-    topic_list=[['lunch','food','dinner','breakfast'],['staff','personnel']]
+    topic_list = [['lunch','food','dinner','breakfast'],['staff','personnel']]
     for l in topic_list:
         if topic in l :
             return l
@@ -37,31 +39,38 @@ sentences=[]
 
 def sent_token(tokens):
     topics = topic_finder()
-    #print "topics: ",topics
-    for token in tokens:
+    for sentence in tokens:
         not_letters_or_numbers = u"!,.?()-'$"
         table = dict((ord(char), u' ') for char in not_letters_or_numbers)
-        token=token.translate(table)
+        token = sentence.translate(table)
         for key_word in topics:
             if key_word in token:
                 sentences.append(token)
                 break
 
 for review in reviews:
-    content=review['Content']
-    tokens=sent_tokenize(content)
-    tokens=[token.lower() for token in tokens]
+    content = review['Content']
+    tokens = sent_tokenize(content)
+    tokens = [t.lower() for t in tokens]
     sent_token(tokens)
 
-for sent in sentences:
-    words=sent.strip().split()
-    posCnt=0
+for sentence in sentences:
+    print sentence
+    words = sentence.strip().split()
+    posCnt = 0
+    multiplier = 1
     for word in words:
-        if word in posWords:
+        if word in intensifiers:
+            multi = intensifiers.index(word)
+            multiplier = intensifierVals[multi]
+            print "multiplier: ",word," value: ", multiplier
+        elif word in posWords:
             i = posWords.index(word)
             val = posVals[i]
-            print "word: ",word," Value: ", val
-        if word in intesifiers:
-            multi = intesifiers.index(word)
-            multi
+            pos_val = val*multiplier
+            print "positive word: ",word," value: ", pos_val
+            pos_count = pos_count + 1
+            #print "positive count: ", pos_count
+            total_count= total_count + 1
+            #print "Total count: ", total_count
 
